a0c77a2c4fe8c1986d0c129bdd0df1f1d2346ab8
Mircio <mfsn@cesar.school>
2025-08-18 15:17:06 -0300

    terminando atividade1

--- 1 atv linked list/5.c
+++ 1 atv linked list/5.c
@@ -11,6 +11,39 @@ typedef struct Node{
     struct Node *next;
 }Node;
 
+void popRelative(Node **head, int posi){
+    Node *temp,*aux=*head;
+    if(posi < 1 || *head==NULL){
+        printf("posi invalida ou lista vazia\n");
+        return;
+    }
+    //remocao no comeco
+    if(posi==1){
+        temp=*head;
+        *head=(*head)->next;
+        free(temp);
+        return;
+    }
+    int cont=0;
+    while(aux!=NULL) {
+        aux=aux->next;
+        cont++;
+    }
+    if(posi > cont){
+        printf("posicao invalida, elementos da lista: %d\n",cont);
+        return;
+    }
+    aux=*head;
+    cont=1;
+    while(cont != posi-1){
+        cont++;
+        aux=aux->next;
+    }
+    temp=aux->next;
+    aux->next=aux->next->next;
+    free(temp);
+}
+
 int main(){
 
     Node *head=NULL;
