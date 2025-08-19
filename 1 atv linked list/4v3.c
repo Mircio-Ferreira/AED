9fad8c6e44afd45dc9f5b7866e89714577109377
Mircio-Ferreira <mfsn@cesar.school>
2025-08-19 12:15:58 -0300

    enviando parte da v3

---
+++ 1 atv linked list/4v3.c
@@ -0,0 +1,27 @@
+/*
+04 - Crie uma nova função que permita a inserção de um nó em
+ uma determinada posição da lista.
+Obs.: essa posição deve ser passada como parâmetro da função.
+*/
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct Node{
+    int n;
+    struct Node *next;
+}Node;
+
+void insertRelative(Node **head, int posi , int n){
+    if(posi<0) return;
+    Node *new =malloc(sizeof(Node));
+    new->n=n;
+    new->next=NULL;
+    
+}
+
+int main(){
+
+
+
+    return 0;
+}
