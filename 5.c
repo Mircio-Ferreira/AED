499b9f5870079edc0fd5d79f261a125fb5df279d
Mircio-Ferreira <mfsn@cesar.school>
2025-08-24 20:36:22 -0300

    enviando quest de monitoria

---
+++ 5.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main(){
+
+    int len,contPrimo = 0;
+
+    scanf(" %d",&len);
+    int array[len];
+
+    for(int i=0;i<len;i++){
+        scanf(" %d",&array[i]);
+        for(int j=0;j<1000;j++){
+            
+        }
+    }
+
+    for(int i=0;i<len;i++){
+        for(int j=0; j<1000;j++){
+
+        }
+    }
+}
