6c7e87bf6ad70411d03fa57ebd43d0db6189c77e
Mircio-Ferreira <mfsn@cesar.school>
2025-08-28 07:27:54 -0300

    enviando teste

---
+++ 04 aula InsertionSort/1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+void insertionSort(int *array,int len){
+    for(int i=1;i<len;i++){
+        int key=array[i];
+        
+    }
+};
+
+int main(){
+
+    int len =5;
+    int array[5]={5,2,3,4,5};
+
+    return 0;
+}
+
