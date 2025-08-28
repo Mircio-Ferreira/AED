2316c998c02bb38e4a0b4ca4e3e74a6b80351296
Mircio-Ferreira <mfsn@cesar.school>
2025-08-28 07:42:25 -0300

    enviando insertionSort

--- 04 aula InsertionSort/1.c
+++ 04 aula InsertionSort/1.c
@@ -2,15 +2,24 @@
 
 void insertionSort(int *array,int len){
     for(int i=1;i<len;i++){
-        int key=array[i];
-        
+        int j=i;
+        while(array[j]<array[j-1] && j>0){
+            int aux = array[j];
+            array[j]=array[j-1];
+            array[j-1]=aux;
+            j--;
+        }
     }
 };
 
 int main(){
 
-    int len =5;
-    int array[5]={5,2,3,4,5};
+    int len=5;
+    int array[5]={5,4,3,2,1};
+    insertionSort(array,len);
+    for(int i=0;i<5;i++){
+        printf("|%d|",array[i]);
+    }
 
     return 0;
 }
