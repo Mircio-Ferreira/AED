705ad003d619f235d8431809f583fc09a307364a
Mircio <mfsn@cesar.school>
2025-09-01 23:23:39 -0300

    envintando parte da questao 2 da lista 1

--- lista 1/1063.c
+++ lista 1/1063.c
@@ -2,11 +2,41 @@
 #include <stdlib.h>
 
 typedef struct Node{
-    int n;
+    char c;
     struct Node *next;
 }Node;
 
-int main(){
+void push(Node **head,char c);
 
+void pop(Node **head);
+
+int main(){
+    Node *head=NULL;
+    int vagao=-1;
+    while(1){
+        head=NULL;
+        scanf("%d",&vagao);
+        if(vagao==0)break;
+        char entrada[vagao],saida[vagao];
+
+        for(int i=0;i<vagao;i++) scanf(" %c",&entrada[i]);
+        for(int i=0;i<vagao;i++) scanf(" %c",&saida[i]);
+           
+    }
     return 0;
+}
+
+void push(Node **head,char c){
+    Node *new=malloc(sizeof(Node));
+    new->c=c;
+    new->next=*head;
+    *head=new;
+}
+
+void pop(Node **head){
+    if(*head!=NULL){
+        Node *delete=*head;
+        *head=(*head)->next;
+        free(delete);
+    }
 }
