ec057b67e0fda03d83419e33a209baf984ef819a
Mircio <mfsn@cesar.school>
2025-09-01 23:55:18 -0300

    fim da questao 2

--- lista 1/1063.c
+++ lista 1/1063.c
@@ -10,6 +10,8 @@ void push(Node **head,char c);
 
 void pop(Node **head);
 
+char check(Node *head);
+
 int main(){
     Node *head=NULL;
     int vagao=-1;
@@ -17,10 +19,32 @@ int main(){
         head=NULL;
         scanf("%d",&vagao);
         if(vagao==0)break;
+
+        int contEntradas=0,contSaidas=0;
         char entrada[vagao],saida[vagao];
+        int possible=1;
 
         for(int i=0;i<vagao;i++) scanf(" %c",&entrada[i]);
         for(int i=0;i<vagao;i++) scanf(" %c",&saida[i]);
+
+        while(contSaidas<vagao){
+            if(head!=NULL && check(head)==saida[contSaidas]){
+                pop(&head);
+                printf("R");
+                contSaidas++;
+            }
+            else if(contEntradas<vagao){
+                push(&head,entrada[contEntradas]);
+                contEntradas++;
+                printf("I");
+            }
+            else{
+                possible=0;
+                break;
+            }
+        }
+        if(!possible) printf(" Impossible");
+        printf("\n");
            
     }
     return 0;
@@ -39,4 +63,9 @@ void pop(Node **head){
         *head=(*head)->next;
         free(delete);
     }
+}
+
+char check(Node *head){
+    if(head==NULL) return '0';
+    return head->c;
 }
