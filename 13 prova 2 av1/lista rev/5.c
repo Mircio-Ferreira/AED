f264d215285a80f332350cc1eeaba3d2a7d3fc55
Mircio <mfsn@cesar.school>
2025-09-30 19:53:52 -0300

    enviando estudos

--- 13 prova 2 av1/lista rev/5.c
+++ 13 prova 2 av1/lista rev/5.c
@@ -1,8 +1,41 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+// Lista duplamente encadeada circular
+
 typedef struct Node{
     int n;
     struct Node *next;
     struct Node *prev;
 }Node;
+
+void insetBeging(Node **head,Node **tail,int n);
+void insertFinish(Node **head,Node **tail,int n);
+
+void removeBeging(Node **head,Node **tail);
+void removeFinish(Node **head,Node **tail);
+
+void print(Node *head,Node *tail);
+void printReverso(Node *head,Node *tail);
+
+int main(){
+
+    Node *head=NULL,*tail=NULL;
+
+    for(int i=0;i<5;i++){
+        //insertFinish(&head,&tail,i);
+    }
+    //print(head,tail);
+    //printReverso(head,tail);
+
+    return 0;
+}
+
+void insetBeging(Node **head,Node **tail,int n);
+void insertFinish(Node **head,Node **tail,int n);
+
+void removeBeging(Node **head,Node **tail);
+void removeFinish(Node **head,Node **tail);
+
+void print(Node *head,Node *tail);
+void printReverso(Node *head,Node *tail);
