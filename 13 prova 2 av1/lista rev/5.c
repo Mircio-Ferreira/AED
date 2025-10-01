f536da9ae3918eb39909195de1566a1edb4afc1f
Mircio-Ferreira <mfsn@cesar.school>
2025-10-01 09:57:13 -0300

    rev de prova aed

--- 13 prova 2 av1/lista rev/5.c
+++ 13 prova 2 av1/lista rev/5.c
@@ -22,20 +22,90 @@ int main(){
 
     Node *head=NULL,*tail=NULL;
 
-    for(int i=0;i<5;i++){
-        //insertFinish(&head,&tail,i);
-    }
-    //print(head,tail);
-    //printReverso(head,tail);
+    for(int i=0;i<5;i++) insertFinish(&head,&tail,i);
+    print(head,tail);
+    printReverso(head,tail);
+
+    removeBeging(&head,&tail);
+
+    print(head,tail);
+    printReverso(head,tail);
+
+    removeFinish(&head,&tail);
+
+    print(head,tail);
+    printReverso(head,tail);
 
     return 0;
 }
 
-void insetBeging(Node **head,Node **tail,int n);
-void insertFinish(Node **head,Node **tail,int n);
+void insetBeging(Node **head,Node **tail,int n){
+    Node *new=malloc(sizeof(Node));
+    new->n=n;
+    if(*head==NULL) *head=*tail=new->next=new->prev=new;
+    else{
+        new->next=*head;
+        (*head)->prev=new;
+        *head=new;
+        (*head)->prev=*tail;
+        (*tail)->next=*head;
+    }
+}
+void insertFinish(Node **head,Node **tail,int n){
+    Node *new=malloc(sizeof(Node));
+    new->n=n;
+    if(*head==NULL) *head=*tail=new->next=new->prev=new;
+    else{
+        (*tail)->next=new;
+        new->prev=*tail;
+        *tail=new;
+        (*tail)->next=*head;
+        (*head)->prev=*tail;
+    }
+}
 
-void removeBeging(Node **head,Node **tail);
-void removeFinish(Node **head,Node **tail);
+void removeBeging(Node **head,Node **tail){
+    if(*head!=NULL){
+        Node *remove=*head;
+        if(*head==*tail) *head=*tail=NULL;
+        else{
+            *head=(*head)->next;
+            (*head)->prev=*tail;
+            (*tail)->next=*head;
+        }
+        free(head);
+    }
+}
+void removeFinish(Node **head,Node **tail){
+    if(*head!=NULL){
+        Node *remove=*tail;
+        if(*head==*tail) *head=*tail=NULL;
+        else{
+            *tail=(*tail)->prev;
+            (*tail)->next=*head;
+            (*head)->prev=*tail;
+        }
+        free(remove);
+    }
+}
 
-void print(Node *head,Node *tail);
-void printReverso(Node *head,Node *tail);
+void print(Node *head,Node *tail){
+    if(head!=NULL){
+        Node *aux=head;
+        do{
+            printf("|%d|",aux->n);
+            aux=aux->next;
+        }while(aux!=head);
+        printf("\n");
+    }
+}
+void printReverso(Node *head,Node *tail){
+    if(head!=NULL){
+        Node *aux=tail;
+        do{
+            printf("|%d|",aux->n);
+            aux=aux->prev;
+        }while(aux!=tail);
+        printf("\n");
+    }
+}
--- 13 prova 2 av1/lista rev/6.c
+++ 13 prova 2 av1/lista rev/6.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 typedef struct Node{
     int n;
@@ -11,9 +12,17 @@ void sequencia(Node *head, Node *tail);
 
 int len(Node *head,Node *tail);
 
+void print(Node *head);
+
+void insert(Node **head,Node **tail,int n);
+
 int main(){
     Node *head=NULL,*tail=NULL;
-    
+    srand(time(NULL));
+
+    for(int i=0;i<10;i++) insert(&head,&tail,rand()%100);
+    print(head);
+    sequencia(head,tail);
 }
 
 int len(Node *head,Node *tail){
@@ -28,10 +37,64 @@ int len(Node *head,Node *tail){
     return cont;
 }
 
+// void sequencia(Node *head, Node *tail){
+//     int cont=len(head,tail);
+//     if(cont<4) printf("Tamanho da lista é insuficiente :(");
+//     else{
+//         Node *aux=head;
+//         int cont=0;
+//         printf("Numeros que sao de sequencia de impar: ");
+//         do{
+//             if(aux->n%2!=0) cont++;
+//             else cont=0;
+//             if(cont==2){
+//                 cont=1;
+//                 printf("|%d|",aux->next->n);
+//             }
+//             aux=aux->next;
+//         }while(aux!=head);
+//         if(tail->n%2!=0 && head->n%2!=0) printf("|%d|",head->next->n);
+//         printf("\n");
+//     }
+// }
+
 void sequencia(Node *head, Node *tail){
     int cont=len(head,tail);
     if(cont<4) printf("Tamanho da lista é insuficiente :(");
     else{
-        
+        Node *aux=head;
+        printf("Numeros que sao de sequencia de impar: ");
+        do{
+            if(aux->prev->n%2!=0 && aux->prev->prev->n%2!=0) printf("|%d|",aux->n);
+            aux=aux->next;
+        }while(aux!=head);
+        printf("\n");
+    }
+}
+
+void print(Node *head){
+    if(head!=NULL){
+        Node *aux=head;
+        do{
+            printf("|%d|",aux->n);
+            aux=aux->next;
+        }while(aux!=head);
+        printf("\n");
+    }
+}
+
+void insert(Node **head,Node **tail,int n){
+    Node *new=malloc(sizeof(Node));
+    new->n=n;
+    if(*head==NULL) {
+        *head=*tail=new;
+        new->next=new->prev=new;
+    }
+    else{
+        (*tail)->next=new;
+        new->prev=*tail;
+        *tail=new;
+        (*tail)->next=*head;
+        (*head)->prev=new;
     }
 }
