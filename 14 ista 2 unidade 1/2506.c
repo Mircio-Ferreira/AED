d1936ed244678d0fe9b17631668630872bc43e19
Mircio <mfsn@cesar.school>
2025-10-10 19:25:09 -0300

    bee

--- 14 ista 2 unidade 1/2506.c
+++ 14 ista 2 unidade 1/2506.c
@@ -2,15 +2,66 @@
 #include <stdlib.h>
 
 typedef struct Node{
-    int horas;
-    int minutos;
+    int time;
+    int minutes;
+    int critialMinutes;
     struct Node *next;
     struct Node *prev;
 }Node;
 
-void insertEnd(Node **head,Node **tail,int horas,int minutos);
-void checkCritical(Node *head,Node *tail);
+void insertEnd(Node **head,Node **tail,int time,int minutes,int critialMinutes);
+int checkCritical(Node *head,Node *tail);
+void freeList(Node **head,Node **tail);
 
 int main(){
     Node *head=NULL,*tail=NULL;
+    int contPatient;
+    int time,minutos,criticalMinutes;
+
+    while(1){
+        scanf("%d",&contPatient);
+        if(contPatient==0) break;
+        for(int i=0;i<contPatient;i++){
+            scanf("%d %d %d",&time,&minutos,&criticalMinutes);
+            insertEnd(&head,&tail,time,minutos,criticalMinutes);
+        }
+        int critical=checkCritical(head,tail);
+        printf("%d\n",critical);
+        freeList(&head,&tail);
+    }
+}
+
+void insertEnd(Node **head,Node **tail,int time,int minutes,int critialMinutes){
+    Node *new=malloc(sizeof(Node));
+    new->time=time;
+    new->minutes=minutes;
+    new->critialMinutes=critialMinutes;
+    new->next=NULL;
+    if(*head==NULL){
+        *head=*tail=NULL;
+        new->prev=NULL;
+    }
+    else{
+        (*tail)->next=new;
+        new->prev=*tail;
+        *tail=new;
+    }
+}
+
+void freeList(Node **head,Node **tail){
+    if(*head!=NULL){
+        Node *aux=*head, *delete;
+        while(aux!=NULL){
+            delete=aux;
+            aux=aux->next;
+            free(delete);
+        }
+        *head=*tail=NULL;
+    }
+}
+
+int checkCritical(Node *head,Node *tail){
+    int cont=0;
+
+    return cont;
 }
