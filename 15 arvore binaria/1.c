c5dc5703c251e7a4e2640e4a85acac789ce6e12d
Mircio-Ferreira <mfsn@cesar.school>
2025-10-22 04:30:57 -0300

    estudos de remocao de arvore binaria

--- 15 arvore binaria/1.c
+++ 15 arvore binaria/1.c
@@ -18,6 +18,9 @@ void insertTree(Tree **root,int key);
 // ---- Search ----
 int searchTree(Tree *root,int key);
 
+// ---- Remove ----
+void removeTree(Tree **root,int key);
+
 int main(){
 
     Tree *root=NULL;
@@ -103,4 +106,25 @@ int searchTree(Tree *root,int key){
 
     // para compilador nao reclamar
     return 0;
+}
+
+void removeTree(Tree **root,int key){
+    if(*root ==NULL) return;
+    else if( (*root)->key> key ) removeTree(&(*root)->left,key);
+    else if( (*root)->key< key ) removeTree(&(*root)->right,key);
+    else{// achou
+        if((*root)->right== NULL && (*root)->left==NULL){ //sem filhos
+            free(*root);
+            *root=NULL;
+        }
+        else if((*root)->right==NULL ^ (*root)->left==NULL){ //So tem um filho
+            Tree *aux=*root;
+            if( (*root)->right!=NULL ) *root=(*root)->right;
+            else *root=(*root)->left;
+            free(aux);
+        }
+        else{
+
+        }
+    }
 }
